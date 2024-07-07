fe307ed7875a63361e6d1fe0b23b163bd294e8d4
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-07-07 11:52:25 +0900

    Add: eslint-plugin-jsx-a11y

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 import pluginReactJSXRuntime from "eslint-plugin-react/configs/jsx-runtime.js";
+import jsxA11y from "eslint-plugin-jsx-a11y";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
@@ -13,9 +14,6 @@ export default [
         version: "detect",
       },
     },
-    plugins: {
-      pluginReactConfig,
-    },
   },
   { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
   { languageOptions: { globals: globals.browser } },
@@ -23,5 +21,6 @@ export default [
   ...tseslint.configs.recommended,
   pluginReactConfig,
   pluginReactJSXRuntime,
+  jsxA11y.flatConfigs.recommended,
   eslintConfigPrettier,
 ];
